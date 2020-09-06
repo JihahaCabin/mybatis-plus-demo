e4631942ff433dd6d4a9cb2ef23d9740f70c2f35
zhengzhendu <zdzhengzhendu@163.com>
2020-09-06 19:58:11 +0800

    mybatis-plus init

---
+++ src/main/java/com/haha/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.haha.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.haha.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/test/java/com/haha/SampleTest.java
@@ -0,0 +1,24 @@
+package com.haha;
+
+import com.haha.entity.User;
+import com.haha.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class SampleTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelect() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        userList.forEach(System.out::println);
+    }
+
+}
